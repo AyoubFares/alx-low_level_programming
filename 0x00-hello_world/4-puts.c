7841bf163fbbd00f16104ce500d01dccf046d1ff
AyoubFares <ayoubchris95@gmail.com>
2023-06-15 17:45:10 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
  #include<stdio.h>;
+/* A c program that puts */
+
+int main(void)
 {
-	int main( );
-puts ("\"Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
