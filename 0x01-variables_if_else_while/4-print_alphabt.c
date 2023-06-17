b1efddb7852a221561e87ace2a40edd97acc3202
AyoubFares <ayoubchris95@gmail.com>
2023-06-16 19:44:15 -0700

    exp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	while ((ch != e) && (ch != q))
+	{
+		putchar(ch);
+		ch++;
+	}
+		putchar('\n');
+	return (0);
+}
