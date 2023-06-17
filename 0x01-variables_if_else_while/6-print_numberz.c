6551fbc4abcd8febe6f70c6dbff0c70b4c9d2b47
AyoubFares <ayoubchris95@gmail.com>
2023-06-16 20:20:39 -0700

    exp

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - prints single digits
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*converts to ascii*/
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
