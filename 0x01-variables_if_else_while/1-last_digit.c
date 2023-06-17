a28f2849726df09143b43a63b72f601a6c570cdf
AyoubFares <ayoubchris95@gmail.com>
2023-06-16 17:40:37 -0700

    exp

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,8 @@
 /**
  * main - entry point
  *
- * Description: true and false validation
+ * Description: print the value of status:
+ *              greater than, is zero and is not less than 6
  *
  * Return: 0 (success)
 */
@@ -16,6 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
+
 	if (digit > 5)
 	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
